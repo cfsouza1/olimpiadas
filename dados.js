e564c45b275ac927164bd346b95d527da2ade6c2
cfsouza1 <cfsouza1@gmail.com>
2024-09-06 22:58:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é um dos maiores nomes do skate mundial. Com apenas 14 anos, ela já conquistou diversos títulos e se tornou um ícone da modalidade. Sua habilidade e carisma a tornaram uma das atletas mais populares do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza, a Bia Souza, é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, tornando-se um dos maiores nomes do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou múltiplas medalhas olímpicas, incluindo ouro no solo nos Jogos Olímpicos de Paris 2024. Sua versatilidade e força a tornaram uma das principais ginastas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástina ouro"
+    }
+];
